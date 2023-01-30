6674f976b7e65eaca8c1275fc376b4b53106ea15
marcgupta <81474423+marcgupta@users.noreply.github.com>
2023-01-30 23:29:24 +0530

    Add files via upload

---
+++ car.js
@@ -0,0 +1,47 @@
+status = "";
+bottle = "";
+objects = [];
+
+function preload() {
+
+bottle = loadImage("bottle.avif");
+
+}
+
+function setup() {
+
+    canvas = createCanvas(500,350);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    object_Detector.detect(bottle,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+function draw(){
+    image(bottle,0,0,500,350);
+
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("number").innerHTML = "Out of 1 big item cocossd detected " + objects.length +" item";
+            fill("#fc0303");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x - 14, objects[i].y - 175);
+            noFill();
+            stroke("#fc0303");
+            rect(objects[i].x - 14, objects[i].y - 175, objects[i].width - 2326, objects[i].height - 2850);
+        }
+}
+}
---
+++ tvandac.js
@@ -0,0 +1,47 @@
+status = "";
+tv = "";
+objects = [];
+
+function preload() {
+
+tv = loadImage("tv.avif");
+
+}
+
+function setup() {
+
+    canvas = createCanvas(500,350);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    object_Detector.detect(tv,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+function draw(){
+    image(tv,0,0,500,350);
+
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("number").innerHTML = "Out of 1 big item cocossd detected " + objects.length +" item";
+            fill("#fc0303");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x - 14, objects[i].y - 175);
+            noFill();
+            stroke("#fc0303");
+            rect(objects[i].x - 14, objects[i].y - 175, objects[i].width - 2326, objects[i].height - 2850);
+        }
+}
+}
